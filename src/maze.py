34b9da3b2f02d9e7c95b089206bfca757c88244c
Justin <73289612+SjuuSjuu@users.noreply.github.com>
2025-05-19 20:43:26 +0200

    grid change

--- src/maze.py
+++ src/maze.py
@@ -15,7 +15,7 @@ class Maze:
             (0, 0): (-1, False), (0, 1): (-1, False), (0, 2): (-1, False), (0, 3): (40, True),
             (1, 0): (-1, False), (1, 1): (-1, False), (1, 2): (-1, False), (1, 3): (-10, True),
             (2, 0): (-1, False), (2, 1): (-10, False), (2, 2): (-1, False), (2, 3): (-1, False),
-            (3, 0): (-1, False), (3, 1): (-10, False), (3, 2): (-1, False), (3, 3): (-1, False)
+            (3, 0): (-1, True), (3, 1): (-10, False), (3, 2): (-1, False), (3, 3): (-1, False)
         }
         self.actions = {
             0: (-1, 0),  # omhoog
